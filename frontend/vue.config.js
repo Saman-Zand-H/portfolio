17c40cef2303219ef136420ac956c6c00c066a33
DJS <tnsperuse@gmail.com>
2023-08-07 01:51:04 +0330

    entrypoint added for backend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: `http://172.17.0.1:8000`,
+                ws: true,
+                changeOrigin: true
+            },
+            '^/graphql': {
+                target: `http://172.17.0.1:8000`,
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
 })
