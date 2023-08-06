0fa3ec919c8faa0131af31309f07a3902e657451
DJS <tnsperuse@gmail.com>
2023-08-07 00:49:29 +0330

    entrypoint added for backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py loaddata fixtures/cv.json
-gunicorn -d /usr/src/app/backend/gunicorn.config.py conf.wsgi:application
+gunicorn -c /usr/src/app/backend/gunicorn.config.py conf.wsgi:application
