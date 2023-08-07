42675e4eff6c0363244a13b03c9befd0417b0987
DJS <tnsperuse@gmail.com>
2023-08-08 00:27:09 +0330

    readme.md updated

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -19,7 +19,6 @@ env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).resolve().parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
