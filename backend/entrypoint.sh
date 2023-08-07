527f318fbd158803c44068e5fc66095a130fb263
DJS <tnsperuse@gmail.com>
2023-08-07 16:58:55 +0330

    updated .gitignore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,7 +4,7 @@ python manage.py loaddata fixtures/cv.json
 python manage.py loaddata fixtures/users.json
 python manage.py loaddata fixtures/blog.json
 
-if "$ENVIRONMENT" == "production"
+if ("$ENVIRONMENT" == "production")
 then
 	gunicorn -c /usr/src/app/backend/gunicorn.config.py conf.wsgi:application
 else
