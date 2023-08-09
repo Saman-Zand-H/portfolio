f6c11c15b668c31332567802ab1846db2ad2912b
DJS <tnsperuse@gmail.com>
2023-08-10 00:22:01 +0330

    nginx bugs

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,5 @@ if [ "$ENVIRONMENT" = "development" ]
 then
 	python manage.py runserver 0.0.0.0:8000
 else
-	hypercorn --config conf.asgi:application/hypercorn.config.py
+	hypercorn --config hypercorn.config.py conf.asgi:application
 fi
