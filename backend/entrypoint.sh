a42c65adadf09d5f01379cbaa1977cd43ce1da3f
DJS <tnsperuse@gmail.com>
2023-08-10 02:37:32 +0330

    i just hate nginx :)))

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,5 @@ if [ "$ENVIRONMENT" = "development" ]
 then
 	python manage.py runserver 0.0.0.0:8000
 else
-	python manage.py runserver 0.0.0.0:8000
+	daphne -b 0.0.0.0 -e ssl:8000:privateKey=/certs/private.key:certKey=/certs/cert.pem conf.asgi:application
 fi
