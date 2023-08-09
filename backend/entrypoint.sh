47cd62bfcaae0ebaf51dece3df9c6000e43162b5
DJS <tnsperuse@gmail.com>
2023-08-10 02:59:48 +0330

    finally fixed ssl bug with nginx

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,5 +8,5 @@ if [ "$ENVIRONMENT" = "development" ]
 then
 	python manage.py runserver 0.0.0.0:8000
 else
-	daphne -e ssl:8000:privateKey=/certs/private.key:certKey=/certs/cert.pem conf.asgi:application
+	daphne -v 3 -e ssl:8000:privateKey=/certs/private.key:certKey=/certs/cert.pem conf.asgi:application
 fi
