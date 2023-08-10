ca543cde6133de634f16985df4f6e0ab26cb2ee6
DJS <tnsperuse@gmail.com>
2023-08-10 12:24:53 +0330

    gql bug fixed

--- frontend/src/components/HomeBlocks.vue
+++ frontend/src/components/HomeBlocks.vue
@@ -5,7 +5,7 @@
         <div class="flex flex-col gap-3 lg:grid grid-cols-1 w-full lg:grid-cols-2 lg:gap-2">
             <div class="rounded-3xl lg:hidden animate__animated animate__fadeInUp w-full text-xl row-span-1 bg-zinc-800/70 flex justify-between font-semibold p-5 text-white">
                 <router-link :to="{ name: 'home' }" class="font-sans text-white hover:text-zinc-200">SamanZND</router-link>
-                <button type="button" @click.prevent="isActive = !isActive" class="hover:cursor-pointer hover:text-zinc-300 transition-colors duration-300"><i class="fa fa-bars"></i></button>
+                <button type="button" title="Toggle Sidebar" @click.prevent="isActive = !isActive" class="hover:cursor-pointer hover:text-zinc-300 transition-colors duration-300"><i class="fa fa-bars"></i></button>
             </div>
             <div class="bg-transparent flex flex-col gap-3 lg:grid grid-rows-2 xl:grid-rows-6 lg:gap-2">
                 <!-- The big text box at the top -->
@@ -37,11 +37,11 @@
             <div class="w-full flex flex-col gap-2 lg:grid sm:grid-rows-6 bg-transparent items-start">
                 <div class="rounded-3xl hidden lg:flex animate__animated animate__fadeInUp w-full text-xl row-span-1 bg-zinc-800/70 justify-between font-semibold p-5 text-white">
                     <router-link :to="{ name: 'home' }" class="font-sans text-white text-lg md:text-2xl hover:text-zinc-200">SamanZND</router-link>
-                    <button type="button" id="sidebarBurger" @click.prevent="isActive = !isActive" class="hover:cursor-pointer hover:text-zinc-300 transition-colors duration-300"><i class="fa fa-bars"></i></button>
+                    <button type="button" id="sidebarBurger" @click.prevent="isActive = !isActive" title="Toggle Sidebar" class="hover:cursor-pointer hover:text-zinc-300 transition-colors duration-300"><i class="fa fa-bars"></i></button>
                 </div>
                 <div class="flex lg:flex-row flex-col gap-3 w-full h-full row-span-5 bg-transparent text-center">
                     <div class="h-auto w-full animate__animated animate__fadeInUp lg:w-1/2 rounded-3xl overflow-hidden bg-zinc-800 lg:bg-cover" :style="`background-image: url(${cv.image})`">
-                        <img class="lg:hidden w-full h-fit" :src="cv.image">
+                        <img class="lg:hidden w-full h-fit" alt="SamanZnd" :src="cv.image">
                     </div>
                     <div class="rounded-3xl w-full lg:w-1/2 bg-transparent grid grid-cols-1 gap-3">
                         <div class="row-span-1 animate__animated animate__fadeInUp bg-zinc-800/70 rounded-3xl text-white flex justify-between items-center flex-wrap gap-5 px-6 py-6 lg:py-0">
@@ -54,14 +54,14 @@
                                 <b class="text-base md:text-xl">{{ cv.location }}</b>
                             </div>
                             <div class="rounded-3xl overflow-hidden w-full h-full bg-cover" :style="`background-image: url(${require('../assets/mashhadMap.png')})`">
-                                <img class="w-full h-full lg:hidden" src="../assets/mashhadMap.png">
+                                <img class="w-full h-full lg:hidden" alt="Mashhad Map" src="../assets/mashhadMap.png">
                             </div>
                         </div>
                         <div class="row-span-1 animate__animated animate__fadeInUp bg-zinc-800/70 rounded-3xl text-white flex justify-between gap-2 items-center flex-wrap px-6 py-6 lg:py-0">
-                            <a href="https://github.com/saman-zand-h" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-github bg-slate-800 text-3xl lg:text-2xl"></i></a>
-                            <a href="https://linkedin.com/saman-zand-h" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-linkedin bg-blue-900 text-3xl lg:text-2xl"></i></a>
-                            <a href="https://telegram.me/robsaman" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-telegram bg-cyan-700 text-3xl lg:text-2xl"></i></a>
-                            <a href="https://instagram.com/t.n.saman" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-instagram bg-gradient-to-br from-orange-400 to-purple-700 text-3xl lg:text-2xl"></i></a>
+                            <a href="https://github.com/saman-zand-h" aria-label="GitHub" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-github bg-slate-800 text-3xl lg:text-2xl"></i></a>
+                            <a href="https://linkedin.com/saman-zand-h" aria-label="LinkedIn" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-linkedin bg-blue-900 text-3xl lg:text-2xl"></i></a>
+                            <a href="https://telegram.me/robsaman" aria-label="Telegram" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-telegram bg-cyan-700 text-3xl lg:text-2xl"></i></a>
+                            <a href="https://instagram.com/t.n.saman" aria-label="Instagram" class="inline-block"><i class="fa fab rounded-[100%] hover:opacity-90 p-3 lg:p-2 fa-instagram bg-gradient-to-br from-orange-400 to-purple-700 text-3xl lg:text-2xl"></i></a>
                         </div>
                     </div>
                 </div>
@@ -85,7 +85,7 @@
                     class="mt-5"
                 >
                     <swiper-slide class="p-6" v-for="tech in technologies">
-                        <a :href="tech.url" class="rounded-2xl flex items-center justify-center h-fit p-1">
+                        <a :href="tech.url" :aria-label="tech.name" class="rounded-2xl flex items-center justify-center h-fit p-1">
                             <img class="hover:opacity-80 transition-opacity duration-300" width="60" height="70" :src="tech.icon" :alt="tech.name">
                         </a>
                     </swiper-slide>
@@ -97,7 +97,7 @@
                 <span>
                     <h2 class="font-semibold text-lg md:text-2xl">My Blog:</h2>                                                                                                                                                                                                                                                                                                                                                                                                    
                 </span>
-                <router-link :to="{ name: 'blog' }">
+                <router-link :to="{ name: 'blog' }" :aria-label="Blog">
                     <h2 class="font-semibold text-base md:text-xl text-zinc-500 hover:text-zinc-500/80 hover:cursor-pointer">View All</h2>
                 </router-link>
             </div>
@@ -114,7 +114,7 @@
                     </router-link>
                 </div>
                 <div class="mt-2 flex flex-row gap-2 overflow-x-auto">
-                    <router-link :to="{ name: 'project', params: { slug: project.slug } }" class="h-full w-full" v-for="project in projects" :key="project.slug">
+                    <router-link :to="{ name: 'project', params: { slug: project.slug } }" :aria-label="project.slug" class="h-full w-full" v-for="project in projects" :key="project.slug">
                         <ProjectPortfolioCard 
                                 v-if="!isEmpty(project.images)"
                                 :backgroundUrl="project.images[0]?.image" 
