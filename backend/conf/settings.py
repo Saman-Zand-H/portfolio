d81ec4fee4932e266132dc1abe11fcfa2d05db13
DJS <tnsperuse@gmail.com>
2023-08-24 20:41:07 +0330

    formatted with black

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -20,18 +20,19 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(BASE_DIR, 'apps'))
-
+sys.path.append(os.path.join(BASE_DIR, "apps"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY', "django-insecure-knb8h8w!zv%4=4kff2h60)n=-%e(c6sn4j_@s$@cbo2-ee(0u#")
+SECRET_KEY = env.str(
+    "SECRET_KEY", "django-insecure-knb8h8w!zv%4=4kff2h60)n=-%e(c6sn4j_@s$@cbo2-ee(0u#"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', 0)
+DEBUG = env.bool("DEBUG", 0)
 
 ALLOWED_HOSTS = [
     "backend",
@@ -40,7 +41,7 @@ ALLOWED_HOSTS = [
     "whiteelli.tk",
     "samanznd.ir",
     "backend.samanznd.ir",
-    "91.107.129.206"
+    "91.107.129.206",
 ]
 
 SITE_ID = 1
@@ -61,73 +62,69 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    'allauth',
-    'allauth.account',
-    'django_filters',
-    'corsheaders',
-    'mdeditor',
-    'graphene_django',
-    'rest_framework',
-    
-    'users.apps.UsersConfig',
-    'cv',
-    'blog',
-    'api',
-    'gql'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "allauth",
+    "allauth.account",
+    "django_filters",
+    "corsheaders",
+    "mdeditor",
+    "graphene_django",
+    "rest_framework",
+    "users.apps.UsersConfig",
+    "cv",
+    "blog",
+    "api",
+    "gql",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'conf.urls'
+ROOT_URLCONF = "conf.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / "templates"    
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-ASGI_APPLICATION = 'conf.asgi.application'
+ASGI_APPLICATION = "conf.asgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('DB_NAME', 'postgres'),
-        'USER': env.str('DB_USER', 'postgres'),
-        'PASSWORD': env.str('DB_PASSWORD', 'postgres'),
-        'HOST': env.str('DB_HOST', 'db'),
-        'PORT': env.int('DB_PORT', 5432)
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME", "postgres"),
+        "USER": env.str("DB_USER", "postgres"),
+        "PASSWORD": env.str("DB_PASSWORD", "postgres"),
+        "HOST": env.str("DB_HOST", "db"),
+        "PORT": env.int("DB_PORT", 5432),
     }
 }
 
@@ -137,29 +134,29 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-AUTH_USER_MODEL = 'users.UserModel'
+AUTH_USER_MODEL = "users.UserModel"
 
 # Production Configurations
-if env.str("ENVIRONMENT", 'production') == 'production':
+if env.str("ENVIRONMENT", "production") == "production":
     SECURE_SSL_REDIRECT = True
     CSRF_COOKIE_SECURE = True
     CSRF_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
-    
+
     # todo: Content Security Policy
     # CSP_DIRECTIVES = {
     #     'default-src': ["'self'"],
@@ -169,8 +166,8 @@ if env.str("ENVIRONMENT", 'production') == 'production':
     # }
     # CSP_REPORT_ONLY = True
     # CSP_REPORT_URI = "/csp-report/"
-    
-    SECURE_HSTS_SECONDS = env.int('HSTS_AGE', 3600)
+
+    SECURE_HSTS_SECONDS = env.int("HSTS_AGE", 3600)
     SECURE_HSTS_PRELOAD = True
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_BROWSER_XSS_FILTER = True
@@ -180,9 +177,9 @@ if env.str("ENVIRONMENT", 'production') == 'production':
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -194,8 +191,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = "staticfiles"
 
 MEDIA_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = "/media/"
@@ -204,35 +201,68 @@ MEDIA_ROOT = "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # MDEditor configuration
 MDEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': ["undo", "redo", "|",
-                    "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
-                    "h1", "h2", "h3", "h5", "h6", "|",
-                    "list-ul", "list-ol", "hr", "|",
-                    "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime",
-                    "emoji", "html-entities", "pagebreak", "goto-line", "|",
-                    "help", "info",
-                    "||", "preview", "watch", "fullscreen"], 
-        'upload_image_formats': ["jpg", "jpeg", "gif", "png","svg"],
-        'image_folder': 'editor',
-        'theme': 'default', 
-        'preview_theme': 'default',
-        'editor_theme': 'pastel-on-dark', 
-        'toolbar_autofixed': False, 
-        'search_replace': True,
-        'emoji': True, 
-        'tex': True, 
-        'flow_chart': True, 
-        'sequence': True, 
-        'watch': True,
-        'lineWrapping': True, 
-        'lineNumbers': True, 
-        'language': 'en'
+    "default": {
+        "toolbar": [
+            "undo",
+            "redo",
+            "|",
+            "bold",
+            "del",
+            "italic",
+            "quote",
+            "ucwords",
+            "uppercase",
+            "lowercase",
+            "|",
+            "h1",
+            "h2",
+            "h3",
+            "h5",
+            "h6",
+            "|",
+            "list-ul",
+            "list-ol",
+            "hr",
+            "|",
+            "link",
+            "reference-link",
+            "image",
+            "code",
+            "preformatted-text",
+            "code-block",
+            "table",
+            "datetime",
+            "emoji",
+            "html-entities",
+            "pagebreak",
+            "goto-line",
+            "|",
+            "help",
+            "info",
+            "||",
+            "preview",
+            "watch",
+            "fullscreen",
+        ],
+        "upload_image_formats": ["jpg", "jpeg", "gif", "png", "svg"],
+        "image_folder": "editor",
+        "theme": "default",
+        "preview_theme": "default",
+        "editor_theme": "pastel-on-dark",
+        "toolbar_autofixed": False,
+        "search_replace": True,
+        "emoji": True,
+        "tex": True,
+        "flow_chart": True,
+        "sequence": True,
+        "watch": True,
+        "lineWrapping": True,
+        "lineNumbers": True,
+        "language": "en",
     }
 }
-
--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -1,30 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.static import static  
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
 urlpatterns = [
-    path(
-        route='admin/', 
-        view=admin.site.urls
-    ),
-    
-    path(
-        route="mdeditor/",
-        view=include("mdeditor.urls")
-    ),
-    
-    path(
-        route="api/v1/",
-        view=include("api.urls", namespace="api")
-    ),
-    path(
-        route="graphql/v1/",
-        view=include('gql.urls')
-    ),
-    path(
-        route="",
-        view=include("blog.urls")
-    )
+    path(route="admin/", view=admin.site.urls),
+    path(route="mdeditor/", view=include("mdeditor.urls")),
+    path(route="api/v1/", view=include("api.urls", namespace="api")),
+    path(route="graphql/v1/", view=include("gql.urls")),
+    path(route="", view=include("blog.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
