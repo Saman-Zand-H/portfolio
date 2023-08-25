b65ad55f969329274e06a1ac5c9e3e9a2825324d
DJS <tnsperuse@gmail.com>
2023-08-26 00:39:42 +0330

    readme.md updated

--- backend/apps/api/utils.py
+++ backend/apps/api/utils.py
@@ -2,4 +2,4 @@ import random, string
 
 
 def random_str(length: int = 10) -> str:
-    return "".join(random.choices(string.ascii_letters))
+    return "".join(random.choices(string.ascii_letters, k=length))
